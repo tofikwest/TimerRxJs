31f1e9081be7293c28ecbc4e2b283125f3c2734c
Tofik Hasanov <annexcies@gmail.com>
2021-08-11 21:26:16 +0300

    start TZ

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "modern-normalize/modern-normalize.css";
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
